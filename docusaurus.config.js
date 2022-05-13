8e2003d7290c0ecdba46dc75db3fc1c90fefbcda
Den Bekker <69750264+danirod12@users.noreply.github.com>
2022-05-13 18:48:44 +0300

    Fixed edit-page URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/danirod12/ntd-wiki/',
+          editUrl: 'https://github.com/danirod12/ntd-wiki/blob/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
